9c96d8ce33c0cbef22a24ad882c5da472545549b
Anthony <t9haan01@students.oamk.fi>
2020-10-29 17:34:19 +0200

    ready for deployment

--- database.js
+++ database.js
@@ -4,10 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 
 const connection = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DATABASE,
-  password: process.env.DB_PASS,
+  user: process.env.HEROKU_USER,
+  host: process.env.HEROKU_HOST,
+  database: process.env.HEROKU_DATABASE,
+  password: process.env.HEROKU_PASS,
   port: process.env.DB_PORT,
 });
 module.exports = connection;
